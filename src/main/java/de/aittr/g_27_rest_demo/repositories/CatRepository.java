d94be9f5b277663074e1c1feb80ec9b37bb1fe64
Alyona Dyak <miliona19@gmail.com>
2023-12-11 15:06:09 +0100

    Initial commit

---
+++ src/main/java/de/aittr/g_27_rest_demo/repositories/CatRepository.java
@@ -0,0 +1,54 @@
+package de.aittr.g_27_rest_demo.repositories;
+
+import de.aittr.g_27_rest_demo.domain.SimpleCat;
+import org.springframework.stereotype.Repository;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.util.List;
+@Repository
+public class CatRepository implements CrudRepository<SimpleCat> {
+    private File file = new File("cat.txt");
+    private String delimiter = ";";
+
+
+
+    @Override
+    public SimpleCat save(SimpleCat obj) {
+        return null;
+    }
+
+    @Override
+    public SimpleCat getById(int id) {
+        return null;
+    }
+
+    @Override
+    public List<SimpleCat> getAll() {
+        try (BufferedReader reader = new BufferedReader(new FileReader(file))) {
+            return reader.lines()
+                    .map(line -> line.split(delimiter))
+                    .map(array -> new SimpleCat(
+                            Integer.parseInt(array[0]),
+                            Integer.parseInt(array[1]),
+                            array[2],
+                            Double.parseDouble(array[3])
+                    ))
+                    .toList();
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+
+    }
+
+    public static void main(String[] args) {
+        List<SimpleCat> cats = new CatRepository().getAll();
+        System.out.println();
+    }
+
+    @Override
+    public void deleteById(int id) {
+
+    }
+}
